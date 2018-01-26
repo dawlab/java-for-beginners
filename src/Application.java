8e281f7de9a423049e35bdb00cf41974bb910002
Dawid Łabno <dawidlabno@outlook.com>
2018-01-26 14:49:48 +0100

    3. Non-primitives (Strings)

--- src/Application.java
+++ src/Application.java
@@ -1,27 +1,16 @@
 
 public class Application {
-
+	
 	public static void main(String[] args) {
 		
-		//Primitive types
+		int myInt = 7; 
 		
-		byte byteNumber; //1 bajt - zakres od -128 do 127
-		short shortNumber; //2 bajty - zakres od -32 768 do 32 767
-		int intNumber; //4 bajty - zakres od -2 147 483 648 do 2 147 483 647
-		long longNumber; //8 bajtów - zakres od -2^63 do (2^63)-1 (posiadaj¹ przyrostek L, lub l)
+		String myString = "Dawid"; // Typ zmienna  = obiekt (instancja klasy) 
+		String myString2 = "£abno";
 		
-		double doubleNumber; //liczba z przecinkiem krótka
-		float floatNumber; //liczba z przecinkiem d³uga 
-		
-		char myChar = 'A'; //Pojedynczy znak
-		boolean myBool = false; //true lub false 
+		System.out.println(myString + " " + myString2); //Konkatenacja, czyli ³¹czenie stringów
 		
 		
 		
-		
-		
-		
-	
 	}
-
 }
