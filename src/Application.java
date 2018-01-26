7f57a7bcb9b7e55888ba5d07c74266c2d30e0531
Dawid Łabno <dawidlabno@outlook.com>
2018-01-26 14:41:21 +0100

    2. Primitive types

--- src/Application.java
+++ src/Application.java
@@ -3,8 +3,25 @@ public class Application {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
-
+		//Primitive types
+		
+		byte byteNumber; //1 bajt - zakres od -128 do 127
+		short shortNumber; //2 bajty - zakres od -32 768 do 32 767
+		int intNumber; //4 bajty - zakres od -2 147 483 648 do 2 147 483 647
+		long longNumber; //8 bajtów - zakres od -2^63 do (2^63)-1 (posiadaj¹ przyrostek L, lub l)
+		
+		double doubleNumber; //liczba z przecinkiem krótka
+		float floatNumber; //liczba z przecinkiem d³uga 
+		
+		char myChar = 'A'; //Pojedynczy znak
+		boolean myBool = false; //true lub false 
+		
+		
+		
+		
+		
+		
+	
 	}
 
 }
