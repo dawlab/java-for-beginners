f3c0b9db54a5f60753779ab9ff4e22c1e1ca8f3d
Dawid Łabno <dawidlabno@outlook.com>
2018-01-27 22:54:34 +0100

    8. Arrays (numbers)

--- src/Application.java
+++ src/Application.java
@@ -4,24 +4,24 @@ public class Application {
 
 	public static void main(String[] args) {
 		
+		int[] values = new int[4];
+		
 		Scanner input = new Scanner(System.in);
 		int number;
+		int i = 0;
 		
-		System.out.println("Enter a number: ");
-		number = input.nextInt();
-		
-		switch(number) {
-		
-		case 1: 
-			System.out.println("wybra³eœ 1");
-			break;
-		
-		case 2: 
-			System.out.println("wybra³eœ 2");
-			break;
-		default: 
-			System.out.println("Odpowiedz standardowa");
+		while (i < values.length) {
+			
+			System.out.println("Type a number: ");
+			
+			number = input.nextInt();
+			
+			values[i] = number;
+			
+			i += 1;
+			
 		}
 		
+		System.out.println(values[0] + " " + values[1] + " " + values[2] + " " + values[3]);
 	}
 }
