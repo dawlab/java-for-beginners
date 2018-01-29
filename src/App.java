a6c6796398aea64534fe4d943e1b4a1cde5b2462
Dawid Łabno <dawidlabno@outlook.com>
2018-01-29 16:03:03 +0100

    17. Polymorphism

--- src/App.java
+++ src/App.java
@@ -1,31 +1,20 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
- 
+import java.util.Scanner;
+
 public class App {
-  public static void main(String[] args){
-      int tab[] = {1,2,3,4,5};
-      BufferedReader odczyt = new BufferedReader(new InputStreamReader(System.in));
-      int index = -1;
- 
-      System.out.println("Który element tablicy chcesz zobaczyæ: ");
-      boolean czyPoprawne = false;
- 
-      while(!czyPoprawne) {
-          try {
-          index = Integer.parseInt(odczyt.readLine());
-          } catch (NumberFormatException n) { System.out.println("Niepoprawne dane! " +
-                "\n Który element tablicy chcesz zobaczyæ: ");
-          } catch (IOException e) { System.out.println("B³¹d odczytu danych");
-          }
- 
-          czyPoprawne = index == -1? false : true;
-      }
- 
-      try {
-          System.out.println(tab[index-1]);
-      } catch (ArrayIndexOutOfBoundsException e) {
-          System.out.println("Niepoprawny parametr, rozmiar tablicy to: "+tab.length);
-      }
-  }
+	
+	public static void main(String[] args){
+		
+		Plant plant = new Plant();
+		Tree tree = new Tree();
+		
+		Plant plant2 = tree; 
+		
+		plant2.grow();
+		tree.shedLeaves();
+		doGrow(tree);
+	}
+	
+	public static void doGrow(Plant plant) {
+		plant.grow();
+	}
 }
