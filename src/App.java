938bf8e01b146c4b4c1696c333a8b20cf68bc6d3
Dawid Łabno <dawidlabno@outlook.com>
2018-01-29 10:41:45 +0100

    14. inheritance

--- src/App.java
+++ src/App.java
@@ -2,18 +2,53 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
+
+class Punkt2d {
+	int a;
+	int b; 
+	
+	Punkt2d(int a, int b) {
+		this.a = a;
+		this.b = b; 
+	}
+	
+	Punkt2d() {
+		
+	}
+	
+	void showPunkt2d() {
+		System.out.println(a + "," + b);
+	}
+}
+
+class Punkt3d extends Punkt2d {
+	
+	int c; 
+	
+	Punkt3d(int a, int b, int c) {
+		this.a = a;
+		this.b = b; 
+		this.c = c;
+	}
+	
+	Punkt3d() {
+		
+	}
+	
+	void showPunkt3d() {
+		System.out.println(a + "," + b + "," + c);
+	}
+}
  
 public class App{
   public static void main(String[] args) throws FileNotFoundException{
-	  
-	  int liczby[] = {1,2,3,4,5,6,7,8,9,10};
-	  
-	  for (int x : liczby) {
-		  System.out.println(x);
 	 
+	  Punkt2d nowy = new Punkt2d(2,4);
+	  Punkt3d nowy3d = new Punkt3d(16,14,12);
 	  
-	  /*for(int i=0; i<liczby.length; i++) {
-		  System.out.println(liczby[i]);
-	  }*/
+	  nowy.showPunkt2d();
+	  nowy3d.showPunkt3d();
+	  nowy3d.showPunkt2d();
+	  
   }
 }
